84c7bacf4e3c401afcc626ea2f43164665de1425
OmarGamer6 <45121572+OmarGamer6@users.noreply.github.com>
2018-11-17 14:37:45 +0200

    Update spam.js

--- spam.js
+++ spam.js
@@ -6,8 +6,8 @@ const client = new Discord.Client();
 
 
 client.on('ready', async() => {
-var server = "456294249785196555"; // ايدي السيرفر
-var channel = "511268102181355532";// ايدي الروم
+var server = "456294249785196555"; // BL8M
+var channel = "511268102181355532";// gg
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('Darkness Spam ,Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam , Darkness Spam ,  ')
     },305);
